81cef50fdf47b8513cad4da5fa73d2ef80c3be6d
Mukesh Salaria <msalaria@constellant.com>
2023-07-17 13:29:17 +0530

    chart

--- client/src/pages/TestSelection/TestSelection.jsx
+++ client/src/pages/TestSelection/TestSelection.jsx
@@ -15,7 +15,6 @@ import Alert from '@mui/material/Alert';
 import { flushSync } from 'react-dom';
 import OutlinedInput from '@mui/material/OutlinedInput';
 import { axis } from '../../config';
-import MultiChart from '../../components/ChartLayout/ChartLayout';
 
 function TestSelection() {
 	const routeData = useLoaderData();
@@ -860,23 +859,6 @@ function TestSelection() {
 						>
 							<Chart />
 						</Grid>
-						{/* <Grid
-							item
-							xl={12}
-							sx={{
-								mb: 10,
-								mt: 4,
-								flexGrow: 1,
-								'@media print and (min-width: 320px)': {
-									flexGrow: 0
-								},
-								'@media screen and (max-width: 320px)': {
-									flexGrow: 0
-								}
-							}}
-						>
-							<MultiChart/>
-						</Grid> */}
 					</Grid>
 
 					{/* grid */}
